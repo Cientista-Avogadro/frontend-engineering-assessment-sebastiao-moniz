59a7cfe58046b2205b1c0eb978449145e2486d5d
Cientista-Avogadro <cientistaavogadro2019@gmail.com>
2024-08-29 18:33:59 +0100

    Add Configuring for the Medical ContextApi

---
+++ src/contexts/ClinicalStore.tsx
@@ -0,0 +1,39 @@
+import  {createContext, useState} from "react";
+import {ContextDataType, Result} from "../types";
+
+
+const ClinicalStoreContext = createContext<ContextDataType>(
+    {} as ContextDataType
+);
+export const ClinicalStoreProvider = ({children}: any) => {
+    const [MedicalsDate, setMedicalsDate] = useState<Result[]>([] as Result[]);
+    const [currentMedical, setCurrentMedical] = useState<Result>({} as Result);
+    const [pathname, setPathname] = useState("");
+
+    //Cruds
+    const getMedicals = () => {
+        return MedicalsDate;
+    };
+    const createMedical = (_MedicalsDate: Result) => {
+        setMedicalsDate([...MedicalsDate, _MedicalsDate]);
+    };
+
+    return (
+        <ClinicalStoreContext.Provider
+            value={{
+                currentMedical,
+                setCurrentMedical,
+                getMedicals,
+                createMedical,
+                MedicalsDate,
+                setMedicalsDate,
+                pathname,
+                setPathname
+            }}
+        >
+            {children}
+        </ClinicalStoreContext.Provider>
+    );
+};
+
+export default ClinicalStoreContext;
